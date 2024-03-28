117be38496a847906fced8a5cef9fa3903446130
sdharrin <138178989+sdharrin@users.noreply.github.com>
2024-03-27 17:01:40 -0700

    added ElectionSchema

---
+++ models/Election.jsx
@@ -0,0 +1,82 @@
+import mongoose, { Schema } from "mongoose";
+
+const ElectionSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    startTime: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    endtime: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    startDate: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    endDate: {
+      type: Date,
+      required: true,
+    },
+  },
+  {
+    type: {
+      type: String,
+      required: true,
+      enum: ["live", "test"],
+      default: "active",
+    },
+  },
+  {
+    location: {
+      type: String,
+      required: true,
+      enum: ["online", "onsite", "both"],
+      default: "online",
+    },
+  },
+  {
+    anonymity: {
+      type: String,
+      required: true,
+      enum: ["secret", "poll", "show"],
+      default: "secret",
+    },
+  },
+  {
+    resultViewingAdmins: {
+      type: String,
+      required: true,
+      enum: ["after", "during"],
+      default: "after",
+    },
+  },
+  {
+    resultViewingVoters: {
+      type: String,
+      required: true,
+      enum: ["after", "during", "never"],
+    },
+  },
+  {
+    collection: "Elections",
+    timestamps: true,
+  }
+);
+
+const Election =
+  mongoose.models.Election || mongoose.model("Election", ElectionSchema);
+
+export default Election;
